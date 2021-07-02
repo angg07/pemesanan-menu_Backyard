9e09a7f31d917dec543181568b241d3811c520e6
Muhammad Angga <anggasyamsul70@gmail.com>
2021-07-02 22:57:47 +0700

    Add files via upload

---
+++ supervisor/sidebar.php
@@ -0,0 +1,19 @@
+<div class="wrapper">
+    <!-- Sidebar  -->
+    <nav id="sidebar">
+
+        <ul class="list-unstyled components">
+            <span class="title"><img src="../assets/images/img-nav.png" class="navbar-brand" style="width:200px;height:auto;"></span>
+            <span class="title">Keuangan</span>
+            <li>
+                <a href="history-keuangan.php">History Keuangan</a> <br>
+            </li>
+            <span class="title">Pengaturan Akun</span>
+            <li>
+                <a href="ganti-password.php">Ganti Password</a>
+            </li>
+            <li>
+                <a href="logout.php">Keluar</a>
+            </li>
+        </ul>
+    </nav>
